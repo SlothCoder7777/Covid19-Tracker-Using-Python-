639605a1e217a26b71af3154862ea3de40490b4e
Himanshu Jain <129409119+SlothCoder7777@users.noreply.github.com>
2025-07-25 11:54:29 +0530

    Delete covid_tracker directory

--- covid_tracker/covid_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views, api
-
-urlpatterns = [
-    path('', views.dashboard_view, name='dashboard'),
-    path('api/trend/', api.trend_data, name='trend-api')
-]
